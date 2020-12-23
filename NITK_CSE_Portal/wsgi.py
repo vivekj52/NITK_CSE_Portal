4449397fe189896120cbaa6198558414bc03162e
vivekj52 <vivekj52@gmail.com>
2020-12-23 23:56:12 +0530

    Initial Commit

---
+++ NITK_CSE_Portal/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for NITK_CSE_Portal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NITK_CSE_Portal.settings')
+
+application = get_wsgi_application()
