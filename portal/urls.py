898dd5944a417359d3ea6f30e252964d6c5de8d9
vivekj52 <vivekj52@gmail.com>
2020-12-24 13:44:04 +0530

    Added static pages

---
+++ portal/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home', views.home, name='home'),
+    path('programmes', views.programmes, name='programmes'),
+    path('facilities', views.facilities, name='facilities')
+]
---
+++ portal/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+
+def home(request):
+    template = loader.get_template('home.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def programmes(request):
+    template = loader.get_template('programmes.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def facilities(request):
+    template = loader.get_template('facilities.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
