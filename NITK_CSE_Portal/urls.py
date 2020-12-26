f02352e623c008db51b2964f55422b09b6589cd5
surya <suryapg041996@gmail.com>
2020-12-26 18:16:47 +0530

    updated courses module

--- NITK_CSE_Portal/urls.py
+++ NITK_CSE_Portal/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('portal/', include('portal.urls')),
+    path('', include('portal.urls')),
     path('admin/', admin.site.urls),
+    path('courses/',include('courses.urls')),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Undergraduate, Postgraduate, Doctoral
 
-# Create your views here.
+def courses(request):
+    template = loader.get_template('courses.html')
+    undergraduate = Undergraduate.objects.all()
+    postgraduate = Postgraduate.objects.all()
+    doctoral = Doctoral.objects.all()
+
+    context = {
+    	'ug' : undergraduate,
+    	'pg' : postgraduate,
+    	'dc' : doctoral
+    }
+
+    return HttpResponse(template.render(context, request))
